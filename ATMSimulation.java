bcb2cb2af32daaa53e4baebdff976589671e1980
PriyadharshiniRamanujam2007 <priyadharshiniramanujam2007@gmail.com>
2026-07-05 20:01:33 +0530

    Updation

--- ATMSimulation.java
+++ ATMSimulation.java
@@ -8,13 +8,13 @@ class ATM {
     }
 
     public void checkBalance() {
-        System.out.println("Current Balance: ₹" + balance);
+        System.out.println("Current Balance:"+ balance);
     }
 
     public void deposit(double amount) {
         if (amount > 0) {
             balance += amount;
-            System.out.println("₹" + amount + " deposited successfully.");
+            System.out.println( amount + " deposited successfully.");
         } else {
             System.out.println("Invalid amount!");
         }
@@ -27,23 +27,19 @@ class ATM {
             System.out.println("Insufficient Balance!");
         } else {
             balance -= amount;
-            System.out.println("₹" + amount + " withdrawn successfully.");
+            System.out.println(amount + " withdrawn successfully.");
         }
     }
 }
 
 public class ATMSimulation {
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         ATM atm = new ATM(10000); // Initial Balance
-
         int choice;
 
         do {
-            System.out.println("\n===== ATM MENU =====");
+            System.out.println("\nATM MENU ");
             System.out.println("1. Check Balance");
             System.out.println("2. Deposit");
             System.out.println("3. Withdraw");
@@ -59,13 +55,13 @@ public class ATMSimulation {
                     break;
 
                 case 2:
-                    System.out.print("Enter amount to deposit: ₹");
+                    System.out.print("Enter amount to deposit: ");
                     double deposit = sc.nextDouble();
                     atm.deposit(deposit);
                     break;
 
                 case 3:
-                    System.out.print("Enter amount to withdraw: ₹");
+                    System.out.print("Enter amount to withdraw: ");
                     double withdraw = sc.nextDouble();
                     atm.withdraw(withdraw);
                     break;
